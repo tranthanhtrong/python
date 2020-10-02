fd95c8d955d7fd932e1821168c6914c2ecea4e0b
Tran Thanh Trong 2020 <trongttce130169@gmail.com>
2020-10-02 07:36:07 +0700

    Test command

--- auto_report.py
+++ auto_report.py
@@ -1,3 +1,10 @@
 import sys
 
 print("argument {}\nChangelog1\nChangeLog2".format(sys.argv[1]))
+print("argument {}\nChangelog1\nChangeLog2".format(sys.argv[1]))
+print("argument {}\nChangelog1\nChangeLog2".format(sys.argv[1]))
+print("argument {}\nChangelog1\nChangeLog2".format(sys.argv[1]))
+print("argument {}\nChangelog1\nChangeLog2".format(sys.argv[1]))
+print("argument {}\nChangelog1\nChangeLog2".format(sys.argv[1]))
+print("argument {}\nChangelog1\nChangeLog2".format(sys.argv[1]))
+
