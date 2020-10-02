7b51b1844be953913d27d3e225e1cf76647711ac
Tran Thanh Trong 2020 <trongttce130169@gmail.com>
2020-10-02 08:33:02 +0700

    run 1

--- auto_report.py
+++ auto_report.py
@@ -3,4 +3,4 @@ from a1_corr_matrix import correction_matrix
 from a_utils import *
 team_file = getOldDataset()
 correction_matrix(team_file.train, team_file.resultColName,
-                      team_file.listFileTest, 10, 0.1, 0.1)
+                      team_file.listFileTest, 1, 0.1, 0.1)
