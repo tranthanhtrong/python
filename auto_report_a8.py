ebe293e92ef27debf27059bf44fb12ff25f1d2ed
Tran Thanh Trong 2020 <trongttce130169@gmail.com>
2020-10-03 17:27:05 +0700

    A8./ 70:30 - Tự Train Test - SVM to Predict

--- auto_report_a8.py
+++ auto_report_a8.py
@@ -2,4 +2,7 @@ import sys
 from a8_corr_svm_selftest import correction_svm_selftest
 from a_utils import *
 team_file = getOldDataset()
-correction_svm_selftest(team_file.train, team_file.resultColName, 1, 0.1, 0.1)
+print("A8./ 70:30 - Tự Train Test - SVM to Predict")
+correction_svm_selftest(team_file.train, team_file.resultColName, 1, 0.1, IF_Method.UnivariateSelection, 10)
+correction_svm_selftest(team_file.train, team_file.resultColName, 1, 0.1, IF_Method.PearsonCorrelationMatrix, 10)
+correction_svm_selftest(team_file.train, team_file.resultColName, 1, 0.1, IF_Method.FeatureImportance, 10)
