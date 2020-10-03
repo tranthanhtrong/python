3ed142146f7bac64c0ed8e94010abeec90bba330
Tran Thanh Trong 2020 <trongttce130169@gmail.com>
2020-10-03 16:05:33 +0700

    A2./ Tự Train Test - RandomForestClassifier to Predict

--- auto_report_a2.py
+++ auto_report_a2.py
@@ -1,5 +1,9 @@
 import sys
 from a2_corr_selftest import correction_matrix_selftest
 from a_utils import *
+
 team_file = getOldDataset()
-correction_matrix_selftest(team_file.train, team_file.resultColName, 1, 0.1, 0.1)
+print("A2./ Tự Train Test - RandomForestClassifier to Predict")
+correction_matrix_selftest(team_file.train, team_file.resultColName, 1, 0.1, IF_Method.UnivariateSelection, 10)
+correction_matrix_selftest(team_file.train, team_file.resultColName, 1, 0.1, IF_Method.PearsonCorrelationMatrix, 10)
+correction_matrix_selftest(team_file.train, team_file.resultColName, 1, 0.1, IF_Method.FeatureImportance, 10)
